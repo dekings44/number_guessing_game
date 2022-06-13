2945fe46e9e0f2b982ca0370cf03069911ef3169
Kingsley Ihemere <dekings44@gmail.com>
2022-06-13 22:48:24 +0100

    Number guessing game updated

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,7 +1,8 @@
 from random import randint
 
 
-
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
 
 def check_answer(guess, answer):
     if guess > answer:
@@ -10,6 +11,15 @@ def check_answer(guess, answer):
         print(f'Your guessed number {guess} is too low')
     else:
         print(f"You got it! The answer was {answer}")
+
+
+def set_difficulty():
+    level = input('Choose a difficulty level. "easy" or "hard":')
+    if level == 'easy':
+        return EASY_LEVEL_TURNS
+    else:
+        return HARD_LEVEL_TURNS
+
 print('Welcome to the Guessing Game')
 
 print("I am thinking of a number between 1 and 100.")
@@ -18,3 +28,5 @@ print("I am thinking of a number between 1 and 100.")
 answer = randint(1, 100)
 
 guess = int(input('Make a guess:\n'))
+turns = set_difficulty()
+print(f'You have {turns} attempts remaining to guess the correct number.')
